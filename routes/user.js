b739fd793246abd51243ceea927c84c13225e101
yuvalgorodissky <104304481+yuvalgorodissky@users.noreply.github.com>
2023-05-26 18:45:26 +0300

    temp

--- routes/user.js
+++ routes/user.js
@@ -22,7 +22,6 @@ router.use(async function (req, res, next) {
   }
 });
 
-
 /**
  * This path returns the favorites recipes that were saved by the logged-in user
  */
@@ -50,16 +49,12 @@ router.post("/favorite", async (req, res, next) => {
     const user_id = req.session.user_id;
     const recipe_id = req.body.recipeId;
     const isfav = req.body.isfav;
-    await user_utils.favorite(user_id, recipe_id,isfav);
+    await user_utils.favorite(user_id, recipe_id, isfav);
     res.status(200).send("The Recipe favorite successfully saved");
   } catch (error) {
     next(error);
   }
 });
-
-
-
-
 
 router.get("/mine", async (req, res, next) => {
   try {
@@ -77,13 +72,21 @@ router.get("/mine", async (req, res, next) => {
   }
 });
 
-
-
-
-
-
-
-
+router.get("seen", async (req, res, next) => {
+  try {
+    const user_id = req.session.user_id;
+    const recipes_id = await user_utils.getSeenRecipes(user_id);
+    let recipes_id_array = [];
+    recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
+    const results = await recipe_utils.getRecipesPreview(
+      recipes_id_array,
+      user_id
+    );
+    res.status(200).send(results);
+  } catch (error) {
+    next(error);
+  }
+});
 
 /**
  * This path set the recipe as seen by the user
--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -1,13 +1,10 @@
 const DButils = require("./DButils");
 
-
-
-
 async function favorite(user_id, recipe_id, isfav) {
   if (isfav) {
-  await DButils.execQuery(
-    `insert into favorites values ('${user_id}',${recipe_id},CURDATE())`
-  );
+    await DButils.execQuery(
+      `insert into favorites values ('${user_id}',${recipe_id},CURDATE())`
+    );
   } else {
     await DButils.execQuery(
       `delete from favorites where user_id='${user_id}' and recipe_id=${recipe_id}`
@@ -28,34 +25,30 @@ async function getMyRecipes(user_id) {
   return recipes_id;
 }
 
-
 async function createRecipe(user_id, recipe) {
-  let { name, imageURL, readyInMinutes, vegiterian,vegan, glutenfree } =
+  let { name, imageURL, readyInMinutes, vegiterian, vegan, glutenfree } =
     recipe.previewDetails;
 
   let query = `INSERT INTO recipes (name, imageURL, readyInMinutes, vegiterian, vegan, glutenfree, recipe_date, user_id)
              VALUES ('${name}', '${imageURL}', '${readyInMinutes}', ${vegiterian}, ${vegan}, ${glutenfree}, CURRENT_TIMESTAMP(), '${user_id}');`;
-        
-  let result = await DButils.execQuery(query);  
-  let recipe_id = result.insertId;
 
+  let result = await DButils.execQuery(query);
+  let recipe_id = result.insertId;
 
   let steps = recipe.steps;
   let ingredients = recipe.ingredients;
 
-    
-  for (step in steps){
+  for (step in steps) {
     await DButils.execQuery(
       `INSERT INTO steps (recipe_id, step_number, description) values (${recipe_id}','${step[step_number]}','${steps[description]}')`
     );
   }
-  for (ingredient in ingredients){
-    try{
+  for (ingredient in ingredients) {
+    try {
       await DButils.execQuery(
         `INSERT INTO ingredients (ingredient_id,name, imageURL) values (${ingredient[ingredient_id]}',${ingredient[name]}','${ingredient[imageURL]}')`
       );
-    }
-    catch{
+    } catch {
       //ingredient already exists
     }
 
@@ -66,7 +59,15 @@ async function createRecipe(user_id, recipe) {
   //TODO: add ingredients and instructions
 }
 
+async function getTreeLastSeens(user_id) {
+  const recipes_id = await DButils.execQuery(
+    `select * from recipes inner join seens on recipes.recipe_id=seens.recipe_id where user_id='${user_id}' order by seen_date desc limit 3`
+  );
+  return recipes_id;
+}
+
 exports.favorite = favorite;
 exports.getFavoriteRecipes = getFavoriteRecipes;
 exports.getMyRecipes = getMyRecipes;
 exports.createRecipe = createRecipe;
+exports.getTreeLastSeens = getTreeLastSeens;
